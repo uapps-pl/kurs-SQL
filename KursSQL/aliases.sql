1af53785579c0f34f2f6cfa52f93ca4c5c5a10c3
KM <support@uapps.pl>
2021-10-26 20:34:24 +0200

    aliases

--- KursSQL/aliases.sql
+++ KursSQL/aliases.sql
@@ -3,7 +3,7 @@
 -- site: uapps.pl
 -- email: support@uapps.pl
 ------------------------------------------------
---Przyk³ad 1
+Przyk³ad 1
 
 select * from event_groups
 select id, descr from event_groups
@@ -13,7 +13,7 @@ select eg.id, eg.descr from event_groups eg
 select event_groups.id, eg.descr from event_groups eg
 
 ------------------------------------------------
---Przyk³ad 2
+Przyk³ad 2
 
 select e.id, e.id_group_event, e.descr, eg.id, eg.descr from events e
 join event_groups eg on eg.id = e.id_group_event
@@ -25,7 +25,7 @@ select * from events
 join event_groups on event_groups.id = events.id_group_event
 
 ------------------------------------------------
---Przyk³ad 3
+Przyk³ad 3
 
 --drop table countries
 create table countries(
@@ -71,13 +71,4 @@ from test_item_countries i
 join countries c1 on c1.prefix = i.country1
 join countries c2 on c2.prefix = i.country2
 join countries c3 on c3.prefix = i.country3
-where i.id = 5
-
-------------------------------------------------
---Inne
-
-select *
-from test_item_countries i
-join countries c1 on c1.prefix = i.country1
-join countries c1 on c2.prefix = i.country2
 where i.id = 5
